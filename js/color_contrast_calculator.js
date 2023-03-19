93fe7c3249a7010f6792b3b8799e0953203c6218
zihanm <67378002+zihanm@users.noreply.github.com>
2023-03-19 11:08:24 +0800

    修复了对比度计算时的bug

--- js/color_contrast_calculator.js
+++ js/color_contrast_calculator.js
@@ -1,90 +1,100 @@
 // Get the input element and listen for changes
 const input = document.querySelector('input[type="file"]');
 input.addEventListener('change', function() {
-  // Load the selected image file
-  const file = input.files[0];
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  reader.onload = function() {
-    const img = new Image();
-	img.onload = function() {
+	// Load the selected image file
+	const file = input.files[0];
+	const reader = new FileReader();
+	reader.readAsDataURL(file);
+	reader.onload = function() {
+		// create a preview
+		const img = new Image();
+		img.onload = function() {
 		
-		canvas.width = img.width;
-		canvas.height = img.height;
+			canvas.width = img.width;
+			canvas.height = img.height;
 
-		const ctx = canvas.getContext('2d');
-		ctx.drawImage(img, 0, 0);
-		
-	  }
-    img.src = reader.result;
-    // Wait for the image to load
-    img.addEventListener('load', function() {
-		console.log('analyzing')
-		document.querySelector('#result').innerHTML = '';
-		const colorThief = new ColorThief();
-		// default is 10 colors
-		const colors = colorThief.getPalette(img,2);
-		// var allColors = colorThief.getPalette(img, 2);
-		console.log(colors)
-		var combos = makeCombination(colors)
-		for(let comboIndex in combos){
-			
-			var color1 = combos[comboIndex][0];
-			var color2 = combos[comboIndex][1];
-			// Calculate the lightness of each color
-			var l1 = getLightness(color1)
-			var l2 = getLightness(color2)
-			// Calculate the color contrast
-			var contrast = (l1 + 0.05) / (l2 + 0.05);
-			// Display the results
-			var color1Hex = rgbToHex(color1);
-			var color2Hex = rgbToHex(color2);
-			var contrastRatio = contrast.toFixed(2);
-			// 前端渲染
-			var colorHexs = [color1Hex, color2Hex]
-			var colorRgbs = [combos[comboIndex][0], combos[comboIndex][1]]
-			// console.log(colorHexs)
-			// console.log(colorRgbs)
-			let i = 0
-			while(i < colorHexs.length){
-				let line = '<div class = "colorLine">'
-				let st = '<div class="color_palette" style="background-color:'
-				st += colorHexs[i]
-				st += '"></div>'
-				line += st
-				
-				
-				line += '颜色'
-				line += (i+1).toString()
-				line += ':&nbsp&nbsp'
-				line += colorHexs[i]
-				line += ', RGB('
-				line += colorRgbs[i][0]
-				line += ','
-				line += colorRgbs[i][1]
-				line += ','
-				line += colorRgbs[i][2]
-				line += ')'
-				
-				line += '</div>'
-				
-				document.querySelector('#result').innerHTML += line
-				
-				i += 1
-			}
-			
-			document.querySelector('#result').innerHTML += '<p>对比度: ' + contrastRatio + '</p>'
-			
+			const ctx = canvas.getContext('2d');
+			ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+			ctx.drawImage(img, 0, 0);
 		}
 		
-		
-	});
-  };
+		img.src = reader.result
+		// Wait for the image to load
+		var color_input_listener = []
+		img.addEventListener('load', function() {
+			console.log('analyzing')
+			// 重置结果
+			document.querySelector('#result').innerHTML = ''
+			
+			const colorThief = new ColorThief();
+			// default is 10 colors
+			var colors = colorThief.getPalette(img,2);
+			console.log('all colors: ', colors)
+			var combos = makeCombination(colors)
+			for(let comboIndex in combos){
+				var color1 = combos[comboIndex][0];
+				var color2 = combos[comboIndex][1];
+				// Calculate the lightness of each color
+				var l1 = getLightness(color1)
+				var l2 = getLightness(color2)
+				// Calculate the color contrast
+				var contrast = (Math.max((l1 + 0.05), (l2 + 0.05))) / (Math.min((l1 + 0.05), (l2 + 0.05)));
+				// Display the results
+				var color1Hex = rgbToHex(color1);
+				var color2Hex = rgbToHex(color2);
+				var contrastRatio = contrast.toFixed(2);
+				// 前端渲染
+				var colorHexs = [color1Hex, color2Hex]
+				var colorRgbs = [combos[comboIndex][0], combos[comboIndex][1]]
+				// console.log(colorHexs)
+				// console.log(colorRgbs)
+				let i = 0
+				while(i < colorHexs.length){
+					let line = '<div class = "colorLine">'
+					let st = '<div class="color_palette" style="background-color:'
+					st += colorHexs[i]
+					st += '"></div>'
+					line += st
+					
+					
+					line += '颜色'
+					line += (i+1).toString()
+					line += ':&nbsp&nbsp'
+					// line += colorHexs[i]
+					// line += ', RGB('
+					line += 'RGB('
+					line += colorRgbs[i][0]
+					line += ','
+					line += colorRgbs[i][1]
+					line += ','
+					line += colorRgbs[i][2]
+					line += ')'
+					
+					let color_input = "<input type='color' class='color_input' id='color_input_"
+					color_input += comboIndex
+					color_input += '_'
+					color_input += i
+					color_input += "' value='"
+					color_input += colorHexs[i]
+					color_input += "' />"
+					line += color_input
+					
+					line += '</div>'
+					
+					document.querySelector('#result').innerHTML += line
+					
+					i += 1
+				}
+				
+				document.querySelector('#result').innerHTML += '<p>对比度: ' + contrastRatio + '</p>'
+			}
+		});
+	};
 });
 
 // Helper function to convert RGB to hex
 function rgbToHex(rgb) {
-  return '#' + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
+	return '#' + ((1 << 24) + (rgb[0] << 16) + (rgb[1] << 8) + rgb[2]).toString(16).slice(1);
 }
 
 function rgbAdj(c){
@@ -104,18 +114,17 @@ function makeCombination(colors){
 	const combinations = [];
 	
 	for (let i = 0; i < colors.length; i++) {
-	  for (let j = i + 1; j < colors.length; j++) {
-	    const combination = [colors[i], colors[j]];
+		for (let j = i + 1; j < colors.length; j++) {
+			const combination = [colors[i], colors[j]];
 	
-	    // Add the combination to the list if it doesn't already exist
-	    if (!combinations.some(c => c.toString() === combination.toString())) {
-	      combinations.push(combination);
-	    }
-	  }
-	
-	// Print the list of unique color combinations
+			// Add the combination to the list if it doesn't already exist
+			if (!combinations.some(c => c.toString() === combination.toString())) {
+			combinations.push(combination);
+			}
+		}
 
 	}
-	console.log(combinations);
+	console.log('unique combinations: ', combinations);
 	return combinations
 }
+
